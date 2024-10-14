2c1b8208aab1fa134210421cd347f34305806e35
BURN43 <info@semode.de>
2024-10-14 23:43:54 +0000

    Cookie einstellungen

--- backend/utils/generateTokenAndSetCookie.js
+++ backend/utils/generateTokenAndSetCookie.js
@@ -7,9 +7,9 @@ export const generateTokenAndSetCookie = (res, userId) => {
 
 	res.cookie("token", token, {
 		httpOnly: true,
-		secure: process.env.NODE_ENV === "production",
-		sameSite: "strict",
-		maxAge: 7 * 24 * 60 * 60 * 1000,
+		secure: process.env.NODE_ENV === "production", // Nur über HTTPS in Produktion
+		sameSite: "None",                               // Ermöglicht Cross-Site-Cookies
+		maxAge: 7 * 24 * 60 * 60 * 1000,               // Ablaufzeit auf 7 Tage gesetzt
 	});
 
 	return token;
