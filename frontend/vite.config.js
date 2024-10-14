ed16bcee3aa647a96694102f1bcbbd5d78117c5b
BURN43 <info@semode.de>
2024-10-14 18:03:55 +0000

    neue env configuration in viteconfig

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,10 +1,5 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import * as dotenv from 'dotenv';
-import { resolve } from 'path';
-
-// Load environment variables from the root `.env` file
-dotenv.config({ path: resolve(__dirname, '../.env') });
 
 export default defineConfig(({ mode }) => {
   const isDevelopment = mode === 'development';
