dc384ec390e0a8b56208b29501a8c2d1b1c69ed0
BURN43 <info@semode.de>
2024-10-15 20:00:38 +0000

    Vercel speed insight

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,5 +1,6 @@
 // app/layout.tsx
 import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from '@vercel/speed-insights/next';
 
 export default function RootLayout({
   children,
@@ -13,7 +14,8 @@ export default function RootLayout({
       </head>
       <body>
         {children}
-        <Analytics /> {/* Integrates Vercel Analytics */}
+        <Analytics />      {/* Integrates Vercel Analytics */}
+        <SpeedInsights />  {/* Integrates Vercel Speed Insights */}
       </body>
     </html>
   );
